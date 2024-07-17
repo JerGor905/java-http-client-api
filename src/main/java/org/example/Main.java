b9a1d11f6e2bafd76c19fa731679d589173e70ea
Jeffery Tse <kytseag@ust.hk>
2024-07-17 19:36:33 +0800

    feat: add POST method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,25 @@ import java.net.http.HttpResponse;
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
         // Get
-        HttpRequest request = HttpRequest.newBuilder()
+        HttpRequest getRequest = HttpRequest.newBuilder()
                 .uri(new URI("https://postman-echo.com/get"))
                 .version(HttpClient.Version.HTTP_2)
                 .GET()
                 .build();
-        HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("Status code: " + response.statusCode());
-        System.out.println("body:\n" + response.body());
+        HttpResponse<String> getResponse = HttpClient.newHttpClient()
+                .send(getRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status code: " + getResponse.statusCode());
+        System.out.println("body:\n" + getResponse.body() + "\n");
+
+        // POST
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://postman-echo.com/post"))
+                .headers("Content-Type", "text/plain;charset=UTF-8")
+                .POST(HttpRequest.BodyPublishers.ofString("Sample request body"))
+                .build();
+        HttpResponse<String> postResponse = HttpClient.newHttpClient()
+                .send(postRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status code: " + postResponse.statusCode());
+        System.out.println("body:\n" + postResponse.body());
     }
 }
